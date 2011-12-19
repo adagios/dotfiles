b8a7ac189d37933ebe9d7407682a100d1829871e
Joao Nelas <joao.nelas@gmail.com>
2011-12-19 17:25:32 +0000

    added week param to last-week-log.rb
    By default logs the past week, but can be any n week before

--- bin/last-week-log.rb
+++ bin/last-week-log.rb
@@ -7,8 +7,12 @@ DATE_FMT="%b %d %Y"
 author=`git config --get user.email`.chomp
 today= Date.today
 
-last_week_start = (today - (7 + today.wday)).strftime(DATE_FMT)
-last_week_end = (today - (0 + today.wday)).strftime(DATE_FMT)
+n_weeks_before=ARGV[0].to_i||0
+
+today=today + n_weeks_before * 7
+
+last_week_start = (today - (6 + today.wday)).strftime(DATE_FMT)
+last_week_end = (today - today.wday).strftime(DATE_FMT)
 
 puts "Listing commits by #{author} between #{last_week_start} and #{last_week_end}"
 puts "============================================================================\n\n"
@@ -16,10 +20,9 @@ puts "==========================================================================
 Dir.foreach('.') do |f|
    next unless File.directory? f #only dirs
    next if f =~ /\.+/  #skip . and ..
-   next unless Dir.new(f).include? '.git' #only git repos
 
    Dir.chdir(f) do |dir|
-      output=`git log --all --color --author #{author} --after="#{last_week_start}" --before="#{last_week_end}" --oneline`
+      output=`git log --all --color --author="#{author}" --after="#{last_week_start}" --before="#{last_week_end}" --oneline`
 
       if output.length>0 then
          puts f
@@ -30,4 +33,3 @@ Dir.foreach('.') do |f|
 
    end
 end
-
