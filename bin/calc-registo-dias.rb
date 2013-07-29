484a2108b4465c824bcbe9688f5e57833d4a1b8a
João Nelas <joao.nelas@gmail.com>
2013-07-29 14:27:03 +0100

    script para recolher @effort do NV

---
+++ bin/calc-registo-dias.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'find'
+
+week=ARGV[0]
+projMap = {}
+
+Dir.glob("#{ENV['HOME']}/Library/Application Support/Notational Data/log- #{week}*") do |path|
+   # puts path
+
+   open(path) do |file|
+      file.grep(/@effort\(.*\)/).each do |effline|
+         # puts effline
+
+         match = effline.match(/@effort\((.*)\)/)
+         if(match) then
+            match[1].split(',').each do |term|
+               arr = term.strip.split(":")
+               # puts arr.inspect
+
+               projMap[arr[0]] = 0 unless projMap.has_key?(arr[0])
+               projMap[arr[0]] = projMap[arr[0]] + arr[1].to_f
+            end
+         end
+
+      end
+   end
+
+   # puts "\n"
+end
+
+puts projMap.inspect
+puts "Total: #{projMap.values.reduce(:+)}"
