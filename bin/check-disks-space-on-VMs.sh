c659f286ac03b84282434ca8bdfff8f07ea37fc7
João Nelas <joao.nelas@gmail.com>
2016-12-20 14:09:41 +0000

    better organize servers

--- bin/check-disks-space-on-VMs.sh
+++ bin/check-disks-space-on-VMs.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-SERVERS="df galp galpdistribuicao bpopular vodafone diusframi diusframi-es prod2.lidl everis"
+SERVERS=(df)
 
-for server in $SERVERS; do
+# linodes prod
+SERVERS+=(bpopular galp galpdistribuicao multipla sinergi vodafone)
+# linodes qual
+SERVERS+=(diusframi diusframi-es galp-qual sinergi-qual)
+
+# onprem prod
+SERVERS+=(prod2.lidl dius.es-prod)
+
+for server in "${SERVERS[@]}"; do
    echo ""
    echo "$server"
    echo "==============="
