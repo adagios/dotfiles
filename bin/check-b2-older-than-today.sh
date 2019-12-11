ff4f0997a650a18c1b9a2580118d75975b27f495
João Nelas <joao.nelas@gmail.com>
2019-12-11 13:35:18 +0000

    usa versão b2 do virtualenv

--- bin/check-b2-older-than-today.sh
+++ bin/check-b2-older-than-today.sh
@@ -3,5 +3,5 @@
 . "$(dirname $0)/VMs.rc"
 
 for server in "${SERVERS_LINODE[@]}"; do
-   ssh "root@$server" ". /etc/sysconfig/duplicity-duplicity-to-b2 2>/dev/null; printf \"%-50s : %s\" \"$server\" \"\$(duplicity collection-status \${DUPLICITY_ARGUMENTS[*]} 2>&1 | tail -3 | head -1)\" " | grep -v "$(date +"%b %d")"
+   ssh "root@$server" ". /etc/sysconfig/duplicity-duplicity-to-b2 2>/dev/null; printf \"%-50s : %s\" \"$server\" \"\$(. /opt/duplicity/.env/bin/activate &>/dev/null ; duplicity collection-status \${DUPLICITY_ARGUMENTS[*]} 2>&1 | tail -3 | head -1)\" " | grep -v "$(LC_ALL=us date +"%b %e")"
 done
