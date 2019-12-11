d230c7fe2a4d83ca594bcc97d00b2d5ead6f05b9
João Nelas <joao.nelas@gmail.com>
2019-12-11 13:36:19 +0000

    ignora erros ao ver heapdumps

--- bin/check-heapdumps-on-VMs.sh
+++ bin/check-heapdumps-on-VMs.sh
@@ -4,6 +4,6 @@
 
 echo "# heapdumps"
 for server in "${SERVERS[@]}"; do
-   ssh "root@$server"\
-      -C "find /var/log -name '*.hprof*' -printf '$server: [%CY-%Cm-%Cd %CH:%CM] %k Kb %p\\n' | egrep '.*'"
+   ssh "cob@$server"\
+      -C "find /var/log -name '*.hprof*' -printf '$server: [%CY-%Cm-%Cd %CH:%CM] %k Kb %p\\n' 2>/dev/null | egrep '.*'"
 done
